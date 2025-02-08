223bbc3ba434f390201217e520d8a97530e7a2cf
aishanoor1439 <aisha.noor1439@gmail.com>
2025-02-08 22:44:00 +0100

    Second Commit
    completed!

--- src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -4,6 +4,30 @@ package com.mycompany.mavenproject1;
 public class Mavenproject1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("  ^\t\t     ^\t\t\t^");
+        System.out.println(" /I\\\t\t    /I\\\t\t       /I\\");
+        System.out.println("((&))\t\t   ((^))\t      ((&))");
+        System.out.println(" |.|\t         ((((^))))\t       |.|");
+        System.out.println(" |.|\t        (((((|)))))\t       |.|");
+        System.out.println(" |.|\t        (((((|)))))\t       |.|");
+        System.out.println(" |.|\t         ((((|))))\t       |.|");
+        System.out.println(" |.|\t\t  (((|)))\t       |.|");
+        System.out.println("{\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"}");
+        System.out.println("\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"");
+        System.out.println("|\t\t    __\t\t\t |");
+        System.out.println("|\t\t  _{##}_\t\t |");
+        System.out.println("|  ___\t\t {######}\t\t |");
+        System.out.println("| |###|\t\t {######}\t\t |");
+        System.out.println("| |###|\t\t {######}\t\t |");
+        System.out.println("| =====\t\t {######}\t\t |");
+        System.out.println("|\t\t {######}\t\t |");
+        System.out.println("|\t\t {######}\t\t |");
+        System.out.println("|\t\t_{######}_\t\t |");
+        System.out.println("(@@@@@@@@@@@@@@===========@@@@@@@@@@@@@@@)\t\t\t\t");
+        System.out.println("(+++++++++++++/===========\\++++++++++++++)\t\t\t\t");
+        System.out.println("(%%%%%%%%%%%%/=============\\%%%%%%%%%%%%%)\t\t\t\t");
+        System.out.println("(\"\"\"\"\"\"\"\"\"\"\"/===============\\\"\"\"\"\"\"\"\"\"\"\"\")\t\t\t\t");
+        
+        
     }
 }
